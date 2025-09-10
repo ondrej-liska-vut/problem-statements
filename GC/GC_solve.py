7adfd5ec91c9e82b0ff41d1b9c61b8de156ce0d9
Onddej_Liska <192771@vutbr.cz>
2025-09-10 20:00:43 +0200

    Local search in progress

--- GC/GC_solve.py
+++ GC/GC_solve.py
@@ -1,6 +1,8 @@
 import networkx
 from dataclasses import dataclass
 from typing import Optional, Iterable, Protocol, Self, TextIO, TypeVar, final
+import random
+from collections import defaultdict
 
 from roar_net_api.operations import (
     SupportsApplyMove,
@@ -8,6 +10,9 @@ from roar_net_api.operations import (
     SupportsConstructionNeighbourhood,
     SupportsMoves,
     SupportsLowerBoundIncrement,
+    SupportsLocalNeighbourhood,
+    SupportsRandomMovesWithoutReplacement,
+    SupportsObjectiveValueIncrement
 )
 
 
@@ -20,6 +25,11 @@ class Solution():
         self.not_colored = [i for i, c in enumerate(colors) if c is None]
         self.lb = lb
         self.used_colors = len({c for c in self.colors if c is not None}) 
+        self.color_map = defaultdict(list)
+        for n, c in enumerate(colors):
+            if c is not None:
+                self.color_map[c].append(n)
+        self.now_feasible = self.is_feasible()
 
     # def used_colors(self) -> int:
     #     return len({c for c in self.colors if c is not None})
@@ -27,26 +37,30 @@ class Solution():
     def to_textio(self) -> None:
         pass
 
-    # def conflicts(self) -> int:
-    #     cols = self.colors
-    #     cnt = 0
-    #     for u, v in self.problem.edges:
-    #         cu, cv = cols[u], cols[v]
-    #         if cu is not None and cv is not None and cu == cv:
-    #             cnt += 1
-    #     return cnt
+    def conflicts(self) -> int:
+        cols = self.colors
+        cnt = 0
+        for u, v in self.problem.edges:
+            cu, cv = cols[u], cols[v]
+            if cu is not None and cv is not None and cu == cv:
+                cnt += 1
+        return cnt
     
     def objective_value(self) -> Optional[int]:
         if self.is_feasible:
-            return self.lb
-        return None
+            return self.used_colors
+        return self.used_colors + self.problem.inf_penalty
 
     def is_complete(self) -> bool:
-        return all(c is not None for c in self.colors)
+        #return all(c is not None for c in self.colors)
+        return self.not_colored == []
 
     @property
     def is_feasible(self) -> bool:
-        return self.is_complete() # and self.conflicts() == 0
+        return self.is_complete() and self.conflicts() == 0 
+    
+    def colors_around(self, noode:int) -> set[int]:
+        return {self.colors[neigh] for neigh in self.problem.g.neighbors(noode) if self.colors[neigh] is not None}
     
 # ----------------------------------- Moves -----------------------------------
 
@@ -65,6 +79,7 @@ class AddMove(SupportsApplyMove[Solution], SupportsLowerBoundIncrement[Solution]
             solution.used_colors += 1
             solution.lb = max(solution.lb, solution.used_colors)
         solution.colors[self.n] = self.c
+        solution.color_map[self.c].append(self.n)
         solution.not_colored.remove(self.n)
         return solution
 
@@ -90,6 +105,86 @@ class AddNeighbourhood(SupportsMoves[Solution, AddMove]):
             for c in available_colors:
                 yield AddMove(self, n, c)
 
+@final
+class TwoOptMove(SupportsApplyMove[Solution], SupportsObjectiveValueIncrement[Solution]):
+    def __init__(self, neighbourhood: TwoOptNeighbourhood, ix: int, jx: int):
+        self.neighbourhood = neighbourhood
+        # ix and jx are indices
+        self.ix = ix
+        self.jx = jx
+
+    def apply_move(self, solution: Solution) -> Solution:
+        prob = solution.problem
+        n, ix, jx = prob.n, self.ix, self.jx
+        # Update tour length
+        t = solution.tour
+        solution.lb -= prob.dist[t[ix - 1]][t[ix]] + prob.dist[t[jx - 1]][t[jx % n]]
+        solution.lb += prob.dist[t[ix - 1]][t[jx - 1]] + prob.dist[t[ix]][t[jx % n]]
+        # Update solution
+        solution.tour[ix:jx] = solution.tour[ix:jx][::-1]
+        return solution
+
+    def objective_value_increment(self, solution: Solution) -> float:
+        prob = solution.problem
+        n, ix, jx = prob.n, self.ix, self.jx
+        # Tour length increment
+        t = solution.tour
+        incr = prob.dist[t[ix - 1]][t[jx - 1]] + prob.dist[t[ix]][t[jx % n]]
+        incr -= prob.dist[t[ix - 1]][t[ix]] + prob.dist[t[jx - 1]][t[jx % n]]
+        return incr
+    
+@final
+class OneRecolorMove(SupportsApplyMove[Solution], SupportsObjectiveValueIncrement[Solution]):
+    def __init__(self, neighbourhood, n: int, c: int):
+        self.neighbourhood = neighbourhood
+        # ix and jx are indices
+        self.n = n
+        self.c = c
+
+    def apply_move(self, solution: Solution) -> Solution:
+        prob = solution.problem
+        n, ix, jx = prob.n, self.ix, self.jx
+        # Update blbosti
+
+        # Update solution
+
+        return solution
+
+    def objective_value_increment(self, solution: Solution) -> float:
+        loc_neb = solution.colors_around(self.n)
+        # TODO incremental update of conflicts in neighbourhood
+        if solution.now_feasible:
+            if self.c in loc_neb:
+                return self.problem.inf_penalty # conflict
+            else:
+                return 0 if self.c <= solution.used_colors else 1 # new color
+        else: 
+            return solution.is_feasible
+        # Tour length increment
+
+    
+
+
+
+@final
+class OneRecolorNeighbourhood(
+    SupportsRandomMovesWithoutReplacement[Solution, OneRecolorMove],
+):
+    def __init__(self, problem):
+        self.problem = problem
+
+    def random_moves_without_replacement(self, solution: Solution) -> Iterable[OneRecolorMove]:
+        assert self.problem == solution.problem
+        N = len(solution.colors)
+        # This is only meant to be used as a local neighbourhood, so solution should be feasible
+        assert solution.is_feasible
+
+        for n in random.shuffle(list(range(1, N + 1))):
+            for c in random.shuffle(list(range(1, solution.used_colors + 2))):
+                yield OneRecolorMove(self, n)
+
+
+
 
 # ---------------------------------- Problem --------------------------------
 
@@ -97,14 +192,16 @@ class AddNeighbourhood(SupportsMoves[Solution, AddMove]):
 class Problem(
     SupportsConstructionNeighbourhood[AddNeighbourhood],
     SupportsEmptySolution[Solution],
+    SupportsLocalNeighbourhood[OneRecolorNeighbourhood]
 ):
-    def __init__(self, G: networkx.Graph, name: str):
+    def __init__(self, G: networkx.Graph, name: str,inf_penalty=1000):
         self.name = name
         self.c_nbhood: Optional[AddNeighbourhood] = None
         # self.l_nbhood: Optional[TwoOptNeighbourhood] = None
         mapping = {old: old - 1 for old in G.nodes()}
         G_relabelled = networkx.relabel_nodes(G, mapping)
         self.g = G_relabelled
+        self.inf_penalty = inf_penalty
 
     # def __str__(self) -> str:
     #     out: list[str] = []
